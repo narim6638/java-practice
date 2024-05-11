cbc7ea3f803df946c8c1c776564833925781f217
Narim <narim6638@gmail.com>
2024-05-11 21:49:37 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Hello " + br.readLine());
     }
 }
